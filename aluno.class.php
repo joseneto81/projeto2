d57cd0ffe65aec8cc38ab2bc5df2e2f1d3143456
Jose Neto <jose.neto@riodasostras.rj.gov.br>
2015-11-03 16:49:23 -0200

    Implementando funcionalidade NotaFinal

--- aluno.class.php
+++ aluno.class.php
@@ -1,10 +1,13 @@
 <?php
 class Aluno{
 	private $nome;
+	private $acertos;
+	private $peso = 2;
 
-	public function aluno($nome='')
+	public function aluno($nome='', $acertos='')
 	{	
-		$this->nome = $nome;
+		$this->nome   = $nome;
+		$this->acertos= $acertos;
 	}
 	
 	public function getNome()
@@ -16,9 +19,13 @@ class Aluno{
 	{	
 		$this->nome = $nome;
 	}
+
+	public function NotaFinal()
+	{
+		return ($this->acertos/$this->peso);
+	}
 }
 
-$a = new Aluno();
-$a->setNome("Carlos");
-echo "Aluno ".$a->getNome()."\n";
+$a = new Aluno("Carlos",5);
+echo "Aluno ".$a->getNome()." obteve nota: ".$a->NotaFinal()."\n";
 ?>
